356184876bda65f88fa681509fd0cfb126bfb024
AYUSH4DHAR <69918238+AYUSH4DHAR@users.noreply.github.com>
2021-05-10 17:39:37 +0530

    Update seach interger in a 2d matrix1.java

--- array/seach interger in a 2d matrix1.java
+++ array/seach interger in a 2d matrix1.java
@@ -22,3 +22,18 @@ public boolean searchMatrix(int[][] matrix, int target) {
 	
 	return false;
 }
+///////////////////////////////////////////////////////////2nd solutionD
+public boolean searchMatrix(int[][] matrix, int target) {
+            int i = 0, j = matrix[0].length - 1;
+            while (i < matrix.length && j >= 0) {
+                    if (matrix[i][j] == target) {
+                        return true;
+                    } else if (matrix[i][j] > target) {
+                        j--;
+                    } else {
+                        i++;
+                    }
+                }
+            
+            return false;
+        }
