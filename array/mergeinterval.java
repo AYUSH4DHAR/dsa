5f35daad25b8568bdf1e291c2c37d8993c1ad080
AYUSH4DHAR <69918238+AYUSH4DHAR@users.noreply.github.com>
2021-05-11 11:54:36 +0530

    Create mergeinterval.java

---
+++ array/mergeinterval.java
@@ -0,0 +1,27 @@
+public int[][] merge(int[][] intervals) {
+        if(intervals == null || intervals.length == 0){
+            return new int[0][];
+        }
+		// seperate start and end 
+        int[] start = new int[intervals.length];
+        int[] end = new int[intervals.length];
+        List<int[]> res = new LinkedList<>();
+        for(int i = 0; i < intervals.length; i++){
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
+        }
+		
+		// sort start[] end[] respectively
+        Arrays.sort(start);
+        Arrays.sort(end);
+		
+        int j = 0; // start time
+        for(int i = 0; i < intervals.length - 1; i++) { // iterate end time
+            if(start[i + 1] > end[i]) { // when collapse appears
+                res.add(new int[] {start[j], end[i]}); // record
+                j = i + 1; // move previous start time 
+            }    
+        }
+        res.add(new int[] {start[j], end[intervals.length - 1]}); // add last one
+        return res.toArray(new int[res.size()][]);
+    }
