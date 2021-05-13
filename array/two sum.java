d5170013c872a410848640faa0216c8a4173cb17
AYUSH4DHAR <69918238+AYUSH4DHAR@users.noreply.github.com>
2021-05-13 12:33:50 +0530

    Update two sum.java

--- array/two sum.java
+++ array/two sum.java
@@ -1,1 +1,14 @@
 
+ public int[] twoSum(int[] nums, int target) {
+     HashMap<Integer, Integer> tracker = new HashMap<Integer, Integer>();
+        int len = nums.length;
+        for(int i = 0; i < len; i++){
+            if(tracker.containsKey(target-nums[i])){
+                int left = tracker.get(target-nums[i]);
+                return new int[]{left, i};
+            }else{
+                tracker.put(nums[i], i);
+            }
+        }
+        throw new IllegalArgumentException("match not founf");
+    }
