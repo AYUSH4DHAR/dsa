bde22cbb3e2a913787abd366862ba9e2c816dafc
AYUSH4DHAR <69918238+AYUSH4DHAR@users.noreply.github.com>
2021-05-14 20:48:08 +0530

    Create nth node from  end of ll.java

---
+++ ll/nth node from  end of ll.java
@@ -0,0 +1,29 @@
+wmic bios get serialnumber
+ int getNthFromLast(Node head, int n)
+    {
+        //using two pointers, similar to finding middle element.
+        Node currNode = head , nthNode = head;
+        
+        //traversing first n elements with first pointer.
+        while(n!=0)
+            {
+                if(currNode==null)
+                    return -1;
+                currNode = currNode.next;
+                n--;
+            }
+        //now traversing with both pointers and when first pointer gives null 
+        //we have got the nth node from end in second pointer since the first 
+        //pointer had already traversed n nodes and thus had difference of n 
+        //nodes with second pointer.
+        while(currNode!=null)   
+            {
+                nthNode = nthNode.next;
+                currNode = currNode.next;
+            }
+            
+        //returning the data of nth node from end.
+        if(nthNode!=null)
+            return nthNode.data;
+        else
+            return -1;
