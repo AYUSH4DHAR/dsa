d7bace481a8fd8655f87ebbc770576101ef88e59
AYUSH4DHAR <69918238+AYUSH4DHAR@users.noreply.github.com>
2021-05-27 02:02:30 +0530

    Update issubtree.java

--- tree/issubtree.java
+++ tree/issubtree.java
@@ -12,4 +12,33 @@ private boolean isSame(TreeNode t1, TreeNode t2) { // takes O(n)
   if (t1.val != t2.val) return false;
   return isSame(t1.left, t2.left) && isSame(t1.right, t2.right);
 }
-    
+  
+
+
+
+
+//////////////////////////////////////////////////
+*************************************************
+////////////////////////////////////////////////////
+public boolean isSubtree(TreeNode s, TreeNode t) {
+  // s
+  StringBuilder sb = new StringBuilder();
+  preorder(s, sb);
+  String seqS = sb.toString();
+  // t
+  sb = new StringBuilder();
+  preorder(t, sb);
+  String seqT = sb.toString();
+  
+  return seqS.indexOf(seqT) >= 0;
+}
+
+private void preorder(TreeNode t, StringBuilder sb) {
+  if (t == null) {
+    sb.append("null");
+    return;
+  }
+  sb.append("#" + t.val);
+  preorder(t.left, sb);
+  preorder(t.right, sb);
+}
