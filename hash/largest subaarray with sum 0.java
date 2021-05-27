42d26af39682fd6dc730dc3b37e112a5987c5e46
AYUSH4DHAR <69918238+AYUSH4DHAR@users.noreply.github.com>
2021-05-27 12:01:48 +0530

    Create largest subaarray with sum 0.java

---
+++ hash/largest subaarray with sum 0.java
@@ -0,0 +1,33 @@
+ int maxLen(int arr[], int n)
+    {
+        // Creates an empty hashMap hM
+        HashMap<Integer, Integer> hM = new HashMap<Integer, Integer>();
+
+        int sum = 0;     // Initialize sum of elements
+        int max_len = 0; // Initialize result
+
+        // Traverse through the given array
+        for (int i = 0; i < arr.length; i++)
+        {
+            // Add current element to sum
+            sum += arr[i];
+
+            if (arr[i] == 0 && max_len == 0)
+                max_len = 1;
+
+            if (sum == 0)
+                max_len = i + 1;
+
+            // Look this sum in hash table
+            Integer prev_i = hM.get(sum);
+
+            // If this sum is seen before, then update max_len
+            // if required
+            if (prev_i != null)
+                max_len = Math.max(max_len, i - prev_i);
+            else // Else put this sum in hash table
+                hM.put(sum, i);
+        }
+
+        return max_len;
+    }
