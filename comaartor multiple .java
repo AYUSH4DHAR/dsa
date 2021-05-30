d5bb6757b0394f2c542fd5305b58bd88f0f4de60
AYUSH4DHAR <69918238+AYUSH4DHAR@users.noreply.github.com>
2021-05-30 19:53:40 +0530

    Create comaartor multiple .java

---
+++ comaartor multiple .java
@@ -0,0 +1,12 @@
+449
+
+With Java 8:
+
+Collections.sort(al,Comparator.comparing((Person p)->p.firstName)
+          .thenComparing(p->p.lastName)
+          .thenComparingInt(p->p.age);)
+If you have accessor methods:
+
+Comparator.comparing(Person::getFirstName)
+          .thenComparing(Person::getLastName)
+          .thenComparingInt(Person::getAge);
