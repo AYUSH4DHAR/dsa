af4acdbe53b9cac207f05325b87c6f2aace6b4da
AYUSH4DHAR <69918238+AYUSH4DHAR@users.noreply.github.com>
2021-06-01 14:51:50 +0530

    Update combinationsum2.java

--- backtracking/combinationsum2.java
+++ backtracking/combinationsum2.java
@@ -1,22 +1,25 @@
-public class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-    	Arrays.sort(candidates);
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         getResult(result, new ArrayList<Integer>(), candidates, target, 0);
         
         return result;
     }
-    
-    private void getResult(List<List<Integer>> result, List<Integer> cur, int candidates[], int target, int start){
-    	if(target > 0){
-    		for(int i = start; i < candidates.length && target >= candidates[i]; i++){
-    			cur.add(candidates[i]);
-    			getResult(result, cur, candidates, target - candidates[i], i);
-    			cur.remove(cur.size() - 1);
-    		}//for
-    	}//if
+     private void getResult(List<List<Integer>> result, List<Integer> cur, int candidates[], int target, int start){
+        if(target<0)
+            return;
+    	else if(target > 0){
+    		for(int i = start; i < candidates.length ; i++){
+                if(i > start && candidates[i] == candidates[i-1]) continue;
+    			  cur.add(candidates[i]);
+    			   getResult(result, cur, candidates, target - candidates[i], i+1);
+    			   cur.remove(cur.size() - 1);
+    		}
+    	}
     	else if(target == 0 ){
     		result.add(new ArrayList<Integer>(cur));
-    	}//else if
-    }
+            return;
+}
+     }
 }
