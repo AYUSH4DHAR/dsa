11fb73267b1056065b6bb29bf725dca21300080e
AYUSH4DHAR <69918238+AYUSH4DHAR@users.noreply.github.com>
2021-06-07 00:24:48 +0530

    Create majority elenment n by k times.java

---
+++ array/majority elenment n by k times.java
@@ -0,0 +1,46 @@
+ 
+    static int majorityElement(int a[], int size)
+    {
+        /* Find the candidate for Majority*/
+        int cand = findCandidate(a, size); 
+  
+        /* Print the candidate if it is Majority*/
+        if (isMajority(a, size, cand) == true) 
+            return cand;
+        else
+            return -1;
+    }
+
+
+static int findCandidate(int a[], int size) 
+    { 
+        int maj_index = 0, count = 1; *******************
+        // use linear search to compute candidate for majority element
+        for (int i = 1; i < size; i++) 
+        { 
+            if (a[maj_index] == a[i]) 
+                count++; 
+            else
+                count--; 
+            if (count == 0) 
+            { 
+                maj_index = i; 
+                count = 1; 
+            } 
+        } 
+        return a[maj_index]; 
+    }
+    // Function to check if the candidate occurs more than n/2 times 
+    static boolean isMajority(int a[], int size, int cand) 
+    { 
+        int count = 0; 
+        for (int i = 0; i < size; i++) 
+            if (a[i] == cand) 
+            count++; 
+        if (count > size/2) 
+            return true; 
+        else
+            return false; 
+    }
+
+}
