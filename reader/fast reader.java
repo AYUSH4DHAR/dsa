72145c656c890e2446a76158d9691f78321a5f6b
AYUSH4DHAR <69918238+AYUSH4DHAR@users.noreply.github.com>
2021-06-08 12:18:12 +0530

    Create fast reader.java

---
+++ reader/fast reader.java
@@ -0,0 +1,58 @@
+class Codechef
+{
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+ 
+        public FastReader()
+        {
+            br = new BufferedReader(
+                new InputStreamReader(System.in));
+        }
+ 
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { return Integer.parseInt(next()); }
+ 
+        long nextLong() { return Long.parseLong(next()); }
+ 
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+ 
+        String nextLine()
+        {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+ 
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+   FastReader sc = new FastReader();
+		    int t=sc.nextInt();
+
+	
+		while(t-->0){
+		      
+	          long D = sc.nextLong();
+	          long d =sc.nextLong();
