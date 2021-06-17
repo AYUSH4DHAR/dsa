e045596775bfc0b45d27c5dea98d47a9ea3f0028
AYUSH4DHAR <69918238+AYUSH4DHAR@users.noreply.github.com>
2021-06-17 11:37:53 +0530

    Update literators.java

--- syntax/literators.java
+++ syntax/literators.java
@@ -22,3 +22,16 @@ ListIterator i = list.listIterator();******************
    
    	ListIterator
    Can traverse elements present in Collection both in forward and backward directions.
+***********************************************
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class ArrayToIterable {
+   public static void main(String args[]){
+      Integer[] myArray = {897, 56, 78, 90, 12, 123, 75};
+      Iterator<Integer> iterator = Arrays.stream(myArray).iterator();
+      while(iterator.hasNext()) {
+         System.out.println(iterator.next());
+      }
+   }
+}
