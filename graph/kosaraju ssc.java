62694aed44660196c6e29a287302950f3a3e44a9
AYUSH4DHAR <69918238+AYUSH4DHAR@users.noreply.github.com>
2021-06-17 11:56:34 +0530

    Create kosaraju ssc.java

---
+++ graph/kosaraju ssc.java
@@ -0,0 +1,105 @@
+is detailed Kosaraju’s algorithm.
+1) Create an empty stack ‘S’ and do DFS traversal of a graph. In DFS traversal,
+    after calling recursive DFS for adjacent vertices of a vertex, push the vertex to stack. In the above graph, 
+    if we start DFS from vertex 0, we get vertices in stack as 1, 2, 4, 3, 0.
+2) Reverse directions of all arcs to obtain the transpose graph.
+3) One by one pop a vertex from S while S is not empty. Let the popped vertex be ‘v’. Take v as source and do DFS (call DFSUtil(v)).
+   The DFS starting from v prints strongly connected component of v. In the above example, we process vertices in order 0, 3, 4, 2, 1 (One by one popped from stack).
+  *************************************
+      void printSCCs()
+    {
+        Stack stack = new Stack();
+  
+        // Mark all the vertices as not visited (For first DFS)
+        boolean visited[] = new boolean[V];
+        for(int i = 0; i < V; i++)
+            visited[i] = false;
+  
+        // Fill vertices in stack according to their finishing
+        // times
+        for (int i = 0; i < V; i++)
+            if (visited[i] == false)
+                fillOrder(i, visited, stack);
+  
+        // Create a reversed graph
+        Graph gr = getTranspose();
+  
+        // Mark all the vertices as not visited (For second DFS)
+        for (int i = 0; i < V; i++)
+            visited[i] = false;
+  
+        // Now process all vertices in order defined by Stack
+        while (stack.empty() == false)
+        {
+            // Pop a vertex from stack
+            int v = (int)stack.pop();
+  
+            // Print Strongly connected component of the popped vertex
+            if (visited[v] == false)
+            {
+                gr.DFSUtil(v, visited);
+                System.out.println();
+            }
+        }
+    }
+
+
+    void fillOrder(int v, boolean visited[], Stack stack)
+    {
+        // Mark the current node as visited and print it
+        visited[v] = true;
+  
+        // Recur for all the vertices adjacent to this vertex
+        Iterator<Integer> i = adj[v].iterator();
+        while (i.hasNext())
+        {
+            int n = i.next();
+            if(!visited[n])
+                fillOrder(n, visited, stack);
+        }
+  
+        // All vertices reachable from v are processed by now,
+        // push v to Stack
+        stack.push(new Integer(v));
+    }
+
+
+    void fillOrder(int v, boolean visited[], Stack stack)
+    {
+        // Mark the current node as visited and print it
+        visited[v] = true;
+  
+        // Recur for all the vertices adjacent to this vertex
+        Iterator<Integer> i = adj[v].iterator();
+        while (i.hasNext())
+        {
+            int n = i.next();
+            if(!visited[n])
+                fillOrder(n, visited, stack);
+        }
+  
+        // All vertices reachable from v are processed by now,
+        // push v to Stack
+        stack.push(new Integer(v));
+    }
+
+
+ Graph getTranspose()
+    {
+        Graph g = new Graph(V);
+        for (int v = 0; v < V; v++)
+        {
+            // Recur for all the vertices adjacent to this vertex
+            Iterator<Integer> i =adj[v].listIterator();
+            while(i.hasNext())
+                g.adj[i.next()].add(v);
+        }
+        return g;
+    }
+
+
+
+
+
+
+
