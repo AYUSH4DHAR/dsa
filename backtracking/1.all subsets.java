8abac8b15784309186831c0e26c7fe17c6ef2d73
AYUSH4DHAR <69918238+AYUSH4DHAR@users.noreply.github.com>
2021-07-01 15:52:51 +0530

    Update 1.all subsets.java

--- backtracking/1.all subsets.java
+++ backtracking/1.all subsets.java
@@ -21,3 +21,27 @@ public void helper(List<List<Integer>> res, List<Integer> each, int pos, int[] n
     return;
 }
 }
+****************************************************
+ public static void main(String[] args)
+    {
+        String s = "abcd";
+        findsubsequences(s, ""); // Calling a function
+        System.out.println(al);
+    }
+ 
+    private static void findsubsequences(String s,
+                                         String ans)
+    {
+        if (s.length() == 0) {
+            al.add(ans);
+            return;
+        }
+ 
+        // We add adding 1st character in string
+        findsubsequences(s.substring(1), ans + s.charAt(0));
+ 
+        // Not adding first character of the string
+        // because the concept of subsequence either
+        // character will present or not
+        findsubsequences(s.substring(1), ans);
+    }
