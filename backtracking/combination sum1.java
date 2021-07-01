298c6dbf93212decf71394c14ff600cdb2fc812b
AYUSH4DHAR <69918238+AYUSH4DHAR@users.noreply.github.com>
2021-07-01 17:08:20 +0530

    Update combination sum1.java

--- backtracking/combination sum1.java
+++ backtracking/combination sum1.java
@@ -24,3 +24,35 @@ public class Solution {
     	}
     }
 }
+****************************
+class Solution {
+    int n;
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+           n = candidates.length;
+           List<List<Integer>> result = new ArrayList<List<Integer>>();
+          helper(result, new ArrayList<Integer>(), candidates, target, 0);
+        return result;
+    }
+void helper(List<List<Integer>> res, List<Integer> cur, int arr[], int target, int i){
+        if(i>=n)
+            return;
+        if(target<0)
+             return;
+        if(target==0){
+            res.add(new ArrayList<Integer>(cur));
+            return;
+        }
+         cur.add(arr[i]);  
+         if(target - arr[i]>=0){
+            
+            helper(res,cur,arr,target-arr[i],i);
+         }
+    
+         i++;
+         while(i<n && arr[i]==arr[i-1])i++;
+         cur.remove(cur.size()-1);
+         helper(res,cur,arr,target,i);
+    
+    return;
+}
+}
