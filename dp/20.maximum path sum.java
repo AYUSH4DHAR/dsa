0bfa82aa3257f4a2a7ef053c87ebdbe542069d10
AYUSH4DHAR <69918238+AYUSH4DHAR@users.noreply.github.com>
2021-07-06 13:51:09 +0530

    Update and rename maximum path sum to 20.maximum path sum.java

--- dp/20.maximum path sum.java
+++ dp/20.maximum path sum.java
@@ -27,3 +27,23 @@ class Solution{
         return maxi;
     }
 }
+ minimum path sum
+   public int minPathSum(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        
+        
+        int[][] dp = new int[n][m];
+        dp[0][0] =  grid[0][0] ;
+        for(int i=1;i<m;i++)
+             dp[0][i] = dp[0][i-1]+ grid[0][i];
+        for(int j=1;j<n;j++)
+             dp[j][0] = dp[j-1][0]+ grid[j][0];
+         
+        for(int i=1;i<n;i++)
+            for(int j=1;j<m;j++){
+                dp[i][j]  = Math.min(dp[i-1][j] , dp[i][j-1]) + grid[i][j];
+            }
+        return dp[n-1][m-1];
+    }
+}
