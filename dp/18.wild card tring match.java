209079f9b116846cde6eb07b5f5de70e6c51a8da
AYUSH4DHAR <69918238+AYUSH4DHAR@users.noreply.github.com>
2021-07-06 11:49:47 +0530

    Update and rename wild card tring match.java to 18.wild card tring match.java

--- dp/18.wild card tring match.java
+++ dp/18.wild card tring match.java
@@ -1,3 +1,30 @@
+
+int wildCard(String pattern, String str)
+    {
+        int n=pattern.length(),m=str.length();
+        boolean dp[][]=new boolean[m+1][n+1];
+        dp[0][0]=true;
+        for(int i=1;i<=n;i++){
+         if(pattern.charAt(i-1)=='*')
+         dp[0][i]=dp[0][i-1];
+         else
+         break;
+       }
+        for(int i=1;i<=m;i++){//str
+            for(int j=1;j<=n;j++){
+                if(pattern.charAt(j-1)==str.charAt(i-1)||pattern.charAt(j-1)=='?')
+                dp[i][j]=dp[i-1][j-1];
+                else if(pattern.charAt(j-1)=='*')
+                dp[i][j]=dp[i-1][j]||dp[i][j-1];
+            }
+        }
+        return dp[m][n]?1:0;
+    }
+
+
+
+
+
 static boolean match(String first, String second)
 {
  
@@ -32,24 +59,3 @@ static boolean match(String first, String second)
 }
  
 
-int wildCard(String pattern, String str)
-    {
-        int n=pattern.length(),m=str.length();
-        boolean dp[][]=new boolean[m+1][n+1];
-        dp[0][0]=true;
-        for(int i=1;i<=n;i++){
-         if(pattern.charAt(i-1)=='*')
-         dp[0][i]=dp[0][i-1];
-         else
-         break;
-       }
-        for(int i=1;i<=m;i++){//str
-            for(int j=1;j<=n;j++){
-                if(pattern.charAt(j-1)==str.charAt(i-1)||pattern.charAt(j-1)=='?')
-                dp[i][j]=dp[i-1][j-1];
-                else if(pattern.charAt(j-1)=='*')
-                dp[i][j]=dp[i-1][j]||dp[i][j-1];
-            }
-        }
-        return dp[m][n]?1:0;
-    }
