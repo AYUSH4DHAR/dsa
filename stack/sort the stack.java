b6d15aab4492e2a5a641619437cab5887f654340
AYUSH4DHAR <69918238+AYUSH4DHAR@users.noreply.github.com>
2021-07-07 23:14:58 +0530

    Update sort the stack.java

--- stack/sort the stack.java
+++ stack/sort the stack.java
@@ -17,3 +17,18 @@ public Stack<Integer> sort(Stack<Integer> s)
 		}
 		return s;
 	}
+*********************
+sortStack(stack S)
+    if stack is not empty:
+        temp = pop(S);  
+        sortStack(S); 
+        sortedInsert(S, temp);
+Below algorithm is to insert element is sorted order:
+
+sortedInsert(Stack S, element)
+    if stack is empty OR element > top element
+        push(S, elem)
+    else
+        temp = pop(S)
+        sortedInsert(S, element)
+        push(S, temp)
