ab9634d640ebf7b250e8b3dec800085a0278e5a9
AYUSH4DHAR <69918238+AYUSH4DHAR@users.noreply.github.com>
2021-07-07 20:07:01 +0530

    Update log2.java

--- bit/log2.java
+++ bit/log2.java
@@ -6,3 +6,8 @@ int log2(int x)
     return res; 
 } 
 Logic: We right shift x repeatedly until it becomes 0, meanwhile we keep count on the shift operation. This count value is the log2(x).
+    
+    **********************************************
+    
+    
+    if a number N is a power of 2 then bitwise-AND(&) of N and N-1 will be zero. We can say that N is a power of 2 or not based on the value of N&(N-1).
