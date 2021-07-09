6a824493902f080c94f78f9ee0f81f8db1d6b99c
AYUSH4DHAR <69918238+AYUSH4DHAR@users.noreply.github.com>
2021-07-09 12:10:50 +0530

    Update is sum tree.java

--- tree/is sum tree.java
+++ tree/is sum tree.java
@@ -37,3 +37,31 @@
         return 0;
     }
   
+
+
+
+ static int isSumTree(Node node)
+    {
+        int ls,rs;
+       
+        // If node is NULL or it's a leaf
+        // node then return true
+        if(node == null || (node.left == null && node.right == null))
+        {
+            return 1;
+        }
+       
+        // Get sum of nodes in left and
+        // right subtrees
+        ls = sum(node.left);
+        rs = sum(node.right);
+       
+        // If the node and both of its
+        // children satisfy the property
+        // return 1 else 0
+        if((node.data == ls + rs) && isSumTree(node.left) != 0 && isSumTree(node.right) != 0)
+        {
+            return 1;
+        }
+        return 0;
+    }
